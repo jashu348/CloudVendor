e8d5e8d73168905f78772d90bd56455a0f1d683a
Jashu Kumar <jashukumar@live.com>
2025-07-29 00:46:41 +0530

    Update By Jashu

--- restDemo/src/main/java/com/infy/demo/controller/CloudAPIService.java
+++ restDemo/src/main/java/com/infy/demo/controller/CloudAPIService.java
@@ -5,7 +5,6 @@ import com.infy.demo.model.CloudVendor;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
-
-import com.infy.demo.model.CloudVendor;
 
 @RestController
 @RequestMapping("/cloudvendor")
@@ -33,15 +29,14 @@ public class CloudAPIService {
 	}
 
 	@GetMapping("{vendorId}")
-	public String getCloudVendorDetails(@PathVariable("vendorId") String vendorId) {
-
-		return cloudVendorService.getCloudVendor(vendorId).getVendorName();
+	public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId) {
+		return cloudVendorService.getCloudVendor(vendorId);
 	}
 
 	@GetMapping()
 	public List<CloudVendor> getAllCloudVendorDetails() {
 
-		return cloudVendorService.getAllCloudVendors():
+		return cloudVendorService.getAllCloudVendors();
 
 	}
 
